a3ff6fc069309187c910bc5f5a463efecffca293
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-01-05 17:04:37 +0100

    add category controller

---
+++ src/main/java/guru/springfamework/controllers/CategoryController.java
@@ -0,0 +1,39 @@
+package guru.springfamework.controllers;
+
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.api.v1.model.CategoryListDTO;
+import guru.springfamework.services.CategoryService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/categories/")
+public class CategoryController {
+
+private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<CategoryListDTO> getAllCategories(){
+
+        return new ResponseEntity<CategoryListDTO>(
+          new CategoryListDTO(categoryService.findAll()), HttpStatus.OK);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable("name") String name){
+
+        return new ResponseEntity<CategoryDTO>(
+                categoryService.findByName(name), HttpStatus.OK);
+    }
+
+}
