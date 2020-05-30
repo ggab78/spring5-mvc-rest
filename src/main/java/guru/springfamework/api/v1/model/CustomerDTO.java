87bac6ff470b541ce87a4c1f4e7dcff2e5a9d82b
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-05-30 22:12:26 +0200

    add swagger API and edit metadata and customer controller

--- src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
@@ -1,13 +1,16 @@
 package guru.springfamework.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class CustomerDTO {
 
     private Long id;
+    @ApiModelProperty(value="This is first name", required = true, example = "Tom")
     private String firstname;
+    @ApiModelProperty(value="This is last name", required = true, example = "Jerry")
     private String lastname;
 
     @JsonProperty("customer_url")
--- src/main/java/guru/springfamework/controllers/CustomerController.java
+++ src/main/java/guru/springfamework/controllers/CustomerController.java
@@ -3,13 +3,15 @@ package guru.springfamework.controllers;
 import guru.springfamework.api.v1.model.CustomerDTO;
 import guru.springfamework.api.v1.model.CustomerListDTO;
 import guru.springfamework.services.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
+@Api(description = "This is Customer Controller")
 @Controller
 @AllArgsConstructor
 @RequestMapping(CustomerController.BASE_URL)
@@ -19,6 +21,7 @@ public class CustomerController {
     public static final String BASE_URL="/api/v1/customers";
     private CustomerService customerService;
 
+    @ApiOperation(value = "this will get a list of Customers", notes = "Some notes come here")
     @GetMapping
     public ResponseEntity<CustomerListDTO> getAllCustomers(){
         return new ResponseEntity<CustomerListDTO>(
